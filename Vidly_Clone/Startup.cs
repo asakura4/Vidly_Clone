dd9e6943cfa5ea34882321b9a2dc2dd3cfa2e376
Tzu-Hao (Howard) Wang <twang@beancube.com>
2022-05-29 12:00:37 -0700

    Add user authentication and migration

---
+++ Vidly_Clone/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vidly_Clone.Startup))]
+namespace Vidly_Clone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
