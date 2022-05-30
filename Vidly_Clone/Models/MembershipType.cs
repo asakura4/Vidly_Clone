a2288342893769ca87c63feb437451ba2e8a6685
Tzu-Hao (Howard) Wang <twang@beancube.com>
2022-05-29 18:10:12 -0700

    add section 3 exercise 1: Membership type to list of customers

--- Vidly_Clone/Models/MembershipType.cs
+++ Vidly_Clone/Models/MembershipType.cs
@@ -9,6 +9,8 @@ namespace Vidly_Clone.Models
     {
         public byte Id { get; set; }
         public short SignUpFee { get; set; }
+
+        public string Name { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
     }
