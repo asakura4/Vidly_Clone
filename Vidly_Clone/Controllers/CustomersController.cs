22479a1977c304b355c982d67bc9cc4e360e7d83
Tzu-Hao (Howard) Wang <twang@beancube.com>
2022-05-31 22:49:42 -0700

    before section 5 exercies

--- Vidly_Clone/Controllers/CustomersController.cs
+++ Vidly_Clone/Controllers/CustomersController.cs
@@ -54,6 +54,7 @@ namespace Vidly_Clone.Controllers
             var membershipTypes = _context.MembershipTypes.ToList();
             var viewModel = new CustomerFormViewModel
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
@@ -61,9 +62,19 @@ namespace Vidly_Clone.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
 
+                };
+                return View("CustomerForm", viewModel);
+            }
             if (customer.Id == 0)
             {
                 _context.Customers.Add(customer);
